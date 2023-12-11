39faeefc22f44f4570675bbe6de9d85cf4a9f9fa
Carlo Palinckx <carlopalinckx@gmail.nl>
2023-12-11 21:27:27 +0100

    day 4.2 2023

--- 2023/4.test.js
+++ 2023/4.test.js
@@ -9,10 +9,10 @@ Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
-    13,
+    30,
   ],
-  [input, 556057],
-])("Day 3", (input, expected) => {
+  [input, 9236992],
+])("Day 4", (input, expected) => {
   // [
   // 	[ '41 48 83 86 17 ', ' 83 86  6 31 17  9 48 53' ],
   // 	[ '13 32 20 16 61 ', ' 61 30 68 82 17 32 24 19' ],
@@ -22,7 +22,7 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
   // 	[ '31 18 13 56 72 ', ' 74 77 10 23 35 67 36 11' ]
   //   ]
 
-  const games = input
+  const cards = input
     .replace(/\s\s+/g, " ")
     .split("\n")
     .map((line) => {
@@ -32,16 +32,24 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
         .map((game) => game.split(" "));
     });
 
-  const total = games.reduce((total, [left, right]) => {
-    const gamePoints = right.reduce((points, number) => {
-      if (left.includes(number)) {
-        return points === 0 ? 1 : points * 2;
-      }
+  const getWinners = ([left, right]) => {
+    return right.filter((x) => left.includes(x));
+  };
 
-      return points;
-    }, 0);
+  const countCopies = (index) => {
+    const winners = getWinners(cards[index]);
 
-    return total + gamePoints;
+    let count = 1;
+
+    for (let i = 1; i <= winners.length; i++) {
+      count += countCopies(index + i);
+    }
+
+    return count;
+  };
+
+  const total = cards.reduce((sum, _card, index) => {
+    return sum + countCopies(index);
   }, 0);
 
   expect(total).toBe(expected);
